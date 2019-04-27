a54e4f6568810e1d1e69aab2db5831ed32aed4d9
johannalundb <32523835+johannalundb@users.noreply.github.com>
2019-04-27 17:32:46 +0200

    fix javascript loaded and disable broken audio for now

--- js/script.js
+++ js/script.js
@@ -1,40 +1,5 @@
 $(document).ready(function(){
-  
-  $("#show-info").click(function(){
-    if ($("#info-box").is(':hidden')) { 
-        $("#info-box").show();
-        $("#show-info").addClass("active");
-    }
-    else {
-      $("#info-box").hide();
-      $("#show-info").removeClass("active");
-    }
-  });
-    // $("#info-box").click(function(){
-    //   $("#info-box").hide();
-    //   $("#show-info").removeClass("active");
-    // })
-
-  $("#sound-on").click(function(){
-    $("#sound-on").hide();
-    $("#sound-off").show();
-    // $("music").prop('muted', false);
-    jQuery('music').prop("muted", true);
-  })
-
-  $("#sound-off").click(function(){
-    $("#sound-off").hide();
-    $("#sound-on").show();
-    jQuery('music').prop("muted", true);
-  })
-})
-
-var myScroll;
-var intFrameWidth = window.innerWidth;
-
-
-function loaded () {
-  myScroll = new IScroll('#wrapper', {
+  var myScroll = new IScroll('#wrapper', {
     startY: -3000,
     startX: -3000,
     scrollY: true,
@@ -60,31 +25,59 @@ function loaded () {
       speedRatioX: 0.4
     }]
   });
-}
 
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
-  capture: false,
-  passive: false
-} : false);
+  $("#show-info").click(function(){
+    if ($("#info-box").is(':hidden')) {
+        $("#info-box").show();
+        $("#show-info").addClass("active");
+    }
+    else {
+      $("#info-box").hide();
+      $("#show-info").removeClass("active");
+    }
+  });
+    // $("#info-box").click(function(){
+    //   $("#info-box").hide();
+    //   $("#show-info").removeClass("active");
+    // })
 
+  $("#sound-on").click(function(){
+    $("#sound-on").hide();
+    $("#sound-off").show();
+    // $("music").prop('muted', false);
+    jQuery('music').prop("muted", true);
+  });
+
+  $("#sound-off").click(function(){
+    $("#sound-off").hide();
+    $("#sound-on").show();
+    jQuery('music').prop("muted", true);
+  })
+
+  // XXX terrible broken, remove?
+  // document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
+  //   capture: false,
+  //   passive: false
+  // } : false);
+});
 
 //sound
-document.onclick = function() {};
+// document.onclick = function() {};
 
-window.onload = function() {
-    var audioMusic = document.querySelector('#music');
-    audioMusic.volume = 0;
-
-    window.addEventListener('scroll', function(event) {
-        var scrollX = this.scrollX;
-        var scrollY = this.scrollY;
-
-        if (scrollX > 100 && scrollX < 400 && scrollY > 100 && scrollY < 300) {
-            audioMusic.volume = 1;
-        } else {
-            audioMusic.volume = 0;
-        }
-
-        console.log(scrollX, scrollY);
-    });
-};
+// window.onload = function() {
+//     var audioMusic = document.querySelector('#music');
+//     audioMusic.volume = 0;
+// 
+//     window.addEventListener('scroll', function(event) {
+//         var scrollX = this.scrollX;
+//         var scrollY = this.scrollY;
+// 
+//         if (scrollX > 100 && scrollX < 400 && scrollY > 100 && scrollY < 300) {
+//             audioMusic.volume = 1;
+//         } else {
+//             audioMusic.volume = 0;
+//         }
+// 
+//         console.log(scrollX, scrollY);
+//     });
+// };
